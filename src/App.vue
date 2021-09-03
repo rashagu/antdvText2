7b98d15c79684b9df621999634492b2eb7112cd7
kousum <1135192606@qq.com>
2021-09-03 16:09:39 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue';
+import { message } from 'ant-design-vue';
+
+message.error({content: '请求已发送但服务端返回状态码非 2xx 的响应', duration: 5});
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
